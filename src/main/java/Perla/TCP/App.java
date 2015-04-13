f2c7d832f526020b14f35b1dc48bdc83283ec560
BaggioAlessandro <alebaggio91@hotmail.it>
2015-04-13 13:25:15 +0200

    configurazione iniziale

---
+++ src/main/java/Perla/TCP/App.java
@@ -0,0 +1,13 @@
+package Perla.TCP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
