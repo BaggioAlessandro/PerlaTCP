5fb41ad3fa2991196d97ddfdffc419d8d028e2f0
luca <baggi.luca@gmail.com>
2015-04-15 16:52:47 +0200

    Inizio implementazione server

--- src/main/java/org/dei/perla/server/Server.java
+++ src/main/java/org/dei/perla/server/Server.java
@@ -1,5 +1,49 @@
 package org.dei.perla.server;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
 public class Server {
+	
+	private Demux demux;	
+	ServerSocketChannel serverSocketChannel;
+	
+	public Server(){
+		demux = new Demux();
+		try {
+			serverSocketChannel = ServerSocketChannel.open();
+			serverSocketChannel.socket().bind(new InetSocketAddress(9999));
+			serverSocketChannel.configureBlocking(false);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void run(){
+		while(true){
+			try {
+				SocketChannel socketChannel = serverSocketChannel.accept();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/*
+	public class MyCommunication extends Thread{
+		
+		SocketChannel socketChannel;
+		
+		public MyCommunication(SocketChannel socketChannel){
+			this.socketChannel = socketChannel;
+		}
+		
+		@Override
+		public void run()
+	}
+	*/
+	
 
 }
